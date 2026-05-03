8b9eea5645b75e57012809e07668130dab74d60c
zeusdereck-hash <zeus.dereck@gmail.com>
2026-05-03 13:58:46 -0600

    Admin

--- tienda/models.py
+++ tienda/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50)
@@ -21,7 +22,13 @@ class Producto(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
     nombre = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2) # Venta
+    
+    # --- Administración Profunda ---
+    costo = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="Costo de Compra")
+    stock = models.IntegerField(default=0, verbose_name="Cantidad en Existencia")
+    # -------------------------------
+    
     codigo = models.CharField(max_length=50, unique=True)
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to='productos/')
@@ -39,8 +46,28 @@ class Producto(models.Model):
         return self.nombre
     
     def save(self, *args, **kwargs):
-        # Generar slug automáticamente si no existe
         if not self.slug:
-            from django.utils.text import slugify
             self.slug = slugify(self.nombre)
         super().save(*args, **kwargs)
+
+class MovimientoCaja(models.Model):
+    TIPO_CHOICES = [('INGRESO', 'Ingreso (+)'), ('GASTO', 'Gasto (-)')]
+    tipo = models.CharField(max_length=7, choices=TIPO_CHOICES)
+    descripcion = models.TextField(verbose_name="Concepto (Ej. Renta, Pago Proveedor)")
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Movimiento de Caja"
+        verbose_name_plural = "Movimientos de Caja"
+
+class Deuda(models.Model):
+    persona = models.CharField(max_length=200, verbose_name="Cliente o Proveedor")
+    monto_total = models.DecimalField(max_digits=10, decimal_places=2)
+    saldo_pendiente = models.DecimalField(max_digits=10, decimal_places=2)
+    yo_debo = models.BooleanField(default=False, verbose_name="¿Es una deuda mía?") 
+    fecha_limite = models.DateField(verbose_name="Fecha de Pago")
+
+    class Meta:
+        verbose_name = "Deuda"
+        verbose_name_plural = "Deudas"
